1258a7c31a68e5b18ffb98e24381899763e9189a
Roy <royorange88@gmail.com>
2022-04-26 09:16:50 +0800

    update boost

--- cmd/maicai/main.go
+++ cmd/maicai/main.go
@@ -13,17 +13,18 @@ import (
 )
 
 var (
-	cookie       = flag.String("cookie", "", "叮咚cookie， 抓包小程序可得")
+	cookie       = flag.String("cookie", "DDXQSESSID=4c165b29f0466c6acbc48ee07b0b992c", "叮咚cookie， 抓包小程序可得")
 	dingdinghook = flag.String("dingding", "", "钉钉机器人")
-	sid          = flag.String("sid", "", "抓包小程序可得")
-	openid       = flag.String("openid", "", "抓包小程序可得")
-	deviceID     = flag.String("device_id", "", "抓包小程序可得")
-	deviceToken  = flag.String("device_token", "", "抓包小程序可得")
-	ua           = flag.String("ua", "", "User-Agent, 抓包小程序可得")
-	boostMode    = flag.Bool("boost", false, "彻底疯狂！！！！！")
+	sid          = flag.String("sid", "4c165b29f0466c6acbc48ee07b0b992c", "抓包小程序可得")
+	openid       = flag.String("openid", "osP8I0f05BPiuikzy0HQeSMubrg4", "抓包小程序可得")
+	deviceID     = flag.String("device_id", "osP8I0f05BPiuikzy0HQeSMubrg4", "抓包小程序可得")
+	deviceToken  = flag.String("device_token", "WHJMrwNw1k/F0qdLNvE01AUwlQtiEc7qol6Nyikv9NlcvvPrivpurzjH084wLhpKazlcN+OgJ5CPOGBpFtSvdQi8BwFXwUa90dCW1tldyDzmauSxIJm5Txg==1487582755342", "抓包小程序可得")
+	ua           = flag.String("ua", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E217 MicroMessenger/6.8.0(0x16080000) NetType/WIFI Language/en Branch/Br_trunk MiniProgramEnv/Mac", "User-Agent, 抓包小程序可得")
+	boostMode    = flag.Bool("boost", true, "彻底疯狂！！！！！")
 )
 
 var globalCart = NewCart()
+var makingOrderProcess = false
 
 type Cart struct {
 	cart *api.CartInfo
@@ -53,10 +54,19 @@ func (cart *Cart) Get() *api.CartInfo {
 func intervalUpdateCart(ddapi *api.API) {
 
 	for {
+		// 下单中不刷新购物车，避免抢单时冲突
+		if makingOrderProcess {
+			continue
+		}
 		log.Println("正在更新购物车详情...")
 		cart, err := ddapi.Cart()
 		if err != nil {
 			log.Println("购物车获取失败", err)
+			if *boostMode && preBoostTime() {
+				time.Sleep(10 * time.Second)
+			} else {
+				time.Sleep(2 * time.Minute)
+			}
 		} else {
 
 			// 勾选有货的商品
@@ -75,12 +85,14 @@ func intervalUpdateCart(ddapi *api.API) {
 					}
 
 				}
+			} else {
+				log.Println("购物车暂无可下单商品")
 			}
-
 			globalCart.Set(cart)
+			time.Sleep(2 * time.Minute)
 		}
 
-		time.Sleep(2 * time.Minute)
+		//time.Sleep(550 * time.Millisecond)
 	}
 }
 
@@ -148,7 +160,7 @@ CheckTime:
 		if len(cart.NewOrderProductList) == 0 {
 			continue
 		}
-
+		log.Println("正在获取运力...")
 		times, err := ddapi.GetMultiReverseTime(cart.NewOrderProductList[0].Products)
 		if err != nil {
 			log.Println("获取运力失败", err)
@@ -158,6 +170,7 @@ CheckTime:
 					for _, time := range day.Times {
 						if !time.FullFlag {
 							reserveTime = time
+							log.Println("预约时间 -> ", time)
 							dingdingbot.Send(context.Background(), reserveTime.SelectMsg)
 
 							goto MakeOrder
@@ -170,7 +183,7 @@ CheckTime:
 		}
 
 		if boostTime() {
-			time.Sleep(1000 * time.Millisecond)
+			time.Sleep(550 * time.Millisecond)
 		} else {
 			time.Sleep(2000 * time.Millisecond)
 		}
@@ -184,20 +197,49 @@ MakeOrder:
 		log.Println("购物车内无可购买商品, 终止下单...")
 		goto CheckTime
 	}
-
+	makingOrderProcess = true
 	checkOrder, err := ddapi.CheckOrder(cart.NewOrderProductList[0])
 	if err != nil {
 		log.Println("检查订单失败", err)
-		goto CheckTime
+		if *boostMode && boostTime() {
+			checkOrderSuccess := false
+			for !checkOrderSuccess {
+				log.Println("重新检查订单", err)
+				checkOrder, err = ddapi.CheckOrder(cart.NewOrderProductList[0])
+				if err != nil {
+					log.Println("检查订单失败", err)
+					time.Sleep(500 * time.Millisecond)
+				} else {
+					checkOrderSuccess = true
+				}
+			}
+		} else {
+			goto CheckTime
+		}
 	}
-
+	log.Println("检查订单成功，开始下单")
 	order, err := ddapi.AddNewOrder(api.PayTypeAlipay, cart, reserveTime, checkOrder)
 	if err != nil {
+		newOrderSuccess := false
 		log.Println("下单失败", err)
-		goto CheckTime
+		if *boostMode && boostTime() {
+			for !newOrderSuccess {
+				log.Println("重新下单", err)
+				order, err = ddapi.AddNewOrder(api.PayTypeAlipay, cart, reserveTime, checkOrder)
+				if err != nil {
+					log.Println("下单失败", err)
+					time.Sleep(500 * time.Millisecond)
+				} else {
+					newOrderSuccess = true
+				}
+			}
+		} else {
+			goto CheckTime
+		}
 	}
 
 	log.Println("下单成功", order)
+	makingOrderProcess = false
 	dingdingbot.Send(context.Background(), "下单成功, 请付款")
 
 	var continueY string
@@ -219,6 +261,19 @@ func boostTime() bool {
 	}
 
 	if now.Hour() == 8 && now.Minute() >= 30 && now.Minute() <= 35 {
+		return true
+	}
+
+	return false
+}
+
+func preBoostTime() bool {
+	now := time.Now()
+	if now.Hour() == 5 && now.Minute() >= 58 {
+		return true
+	}
+
+	if now.Hour() == 8 && now.Minute() >= 28 && now.Minute() <= 30 {
 		return true
 	}
 
